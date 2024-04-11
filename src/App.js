9c53308a21fef10b9e82984f333c711155f21eae
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-04-12 00:34:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const messages = [
+  "Learn it with all thy strengh",
+  "Apply for jobs",
+  "Invest it into your busiiness",
+];
 
 function App() {
+  const [step, setStep] = useState(1);
+
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <div className="messages">
+        Step {step}:{messages[step - 1]}
+      </div>
+
+      <div className="button">
+        <button
+          style={{ backgroundColor: "aqua", color: "black" }}
+          onClick={handlePrevious}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "aqua", color: "black" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+
+      <h2>
+        Progress: <progress value={"65"} max={"100"}></progress>65%
+      </h2>
+      <p>
+        For <mark style={{ color: "red" }}>William</mark>
+      </p>
+      <meter optimum={"40"} value={"50"} high={"80"} max={"100"}>
+        85%
+      </meter>
     </div>
   );
 }
