5a4e5317983c8f1f8543334cdf71a8b96f95f075
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-04-23 07:24:02 +0100

    Update props.learner.js

--- src/Components/props.learner.js
+++ src/Components/props.learner.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PropType from "prop-types";
 
-const pro = ["Biscuit", "Choco", "All"];
+const pro = ["Biscuit", "Choco", "gum"];
 
 function Students() {
   return (
@@ -16,14 +16,19 @@ function Students() {
 
 function StudentDetails(props) {
   const [product, setProduct] = useState(1);
+  function setProducts() {
+    if (product < 3) {
+      setProduct((s) => s + 1);
+    }
+  }
   return (
     <div className="students">
       <h2>Name: {props.name}</h2>
       <p>Age: {props.Age}</p>
       <p>Gender: {props.gender}</p>
       <p>Student: {props.isStudent ? "Yes" : "No"}</p>
-      <button onClick={() => setProduct((s) => s + 1)}>ChangeProduct</button>
-      <p>product: {pro[product + 1]}</p>
+      <button onClick={setProducts}>ChangeProduct</button>
+      <p>product: {pro[product - 1]}</p>
     </div>
   );
 }
