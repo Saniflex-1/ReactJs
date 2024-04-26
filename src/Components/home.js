0f33c8501fe1d32778084a7959f099d7e67ed36d
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-04-26 01:45:08 +0100

    Create home.js

---
+++ src/Components/home.js
@@ -0,0 +1,43 @@
+function HomePage() {
+  return (
+    <>
+      <div className="homeMe">
+        <div className="homeCont">
+          <div className="cont">
+            <h1>
+              Hair hacks <br />
+              for women
+            </h1>
+            <p>
+              Our game chaging semi-permanent glossing <br /> treatments that
+              colours while it conditions.
+            </p>
+            <div className="home-shop">
+              <button className="shop-btn">Shop now</button>
+            </div>
+          </div>
+          <div className="cont">
+            <img src="assets/s1.png" alt="beauty" />
+          </div>
+        </div>
+      </div>
+
+      <div className="homeBottom">
+        <div className="bottomHome">
+          <div className="sec">
+            <p>
+              Explore our color <br /> Community
+            </p>
+          </div>
+          <div className="sec">
+            <p>
+              Explore our color <br /> Community
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
