a623557e7b5965627441d2dab31bc43657c11ba2
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-04-27 12:06:34 +0100

    Create home.color.js

---
+++ src/Components/home.color.js
@@ -0,0 +1,34 @@
+function HomeColor() {
+  return (
+    <>
+      <div className="home-ceal" id="products">
+        <h5>DISCOVER OUR PRODUCTS</h5>
+        <h2>Colour, Conceal, & Skin Care</h2>
+        <h6>
+          Let a world leading skin colour expert <mark>" Codies "</mark> help
+          create natural, <br /> glossy, modern hair that feels like you
+        </h6>
+
+        <div className="img-products">
+          <div className="pro">
+            <img src="assets/ban.jpg" alt="bank" />
+            <span></span>
+            <p>Permanent Color Kits</p>
+          </div>
+          <div className="pro">
+            <img src="assets/ban.jpg" alt="bank" />
+            <span></span>
+            <p>Permanent Color Kits</p>
+          </div>
+          <div className="pro">
+            <img src="assets/ban.jpg" alt="bank" />
+            <span></span>
+            <p>Permanent Color Kits</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeColor;
