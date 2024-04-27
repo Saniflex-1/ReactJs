3983007818cdea650a94d63b3876873c281cde17
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-04-27 12:07:08 +0100

    Update nav.links.js

--- src/Components/nav.links.js
+++ src/Components/nav.links.js
@@ -1,33 +1,40 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 function NavBar() {
+  const [isTog, setIsToggle] = useState(true);
+  function toggle() {
+    setIsToggle((isTog) => !isTog);
+  }
+
   return (
     <>
-      {/* <button className="humberg" onClick={humber}>
-        &times;
-      </button> */}
       {
         <div className="nav">
-          <div className="logo">
+          <div className="logo big">
             <img src="./tog.gif" alt="log" />
           </div>
-          <div className="nav-links">
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">About</a>
-            </li>
-            <li>
-              <a href="/">Contacts</a>
-            </li>
-            <li>
-              <a href="/">Service</a>
-            </li>
-            <li>
-              <a href="/">Products</a>
-            </li>
+          <div className="logo small" onClick={toggle}>
+            <img src="./tog.gif" alt="log" />
           </div>
+          {isTog && (
+            <div className="nav-links">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Contacts</a>
+              </li>
+              <li>
+                <a href="/">Service</a>
+              </li>
+              <li>
+                <a href="#products">Products</a>
+              </li>
+            </div>
+          )}
 
           <div className="search">
             <input type="text" placeholder="search items" />
