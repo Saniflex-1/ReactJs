524f4a50e394d58826f17a9264ff73b72cfc6258
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-05-02 17:03:38 +0100

    Update home.color.js

--- src/Components/home.color.js
+++ src/Components/home.color.js
@@ -5,31 +5,60 @@ function HomeColor() {
         <h5>DISCOVER OUR PRODUCTS</h5>
         <h2>Colour, Conceal, & Skin Care</h2>
         <h6>
-          Let a world leading skin colour expert <mark>" Codies "</mark> help
-          create natural, <br /> glossy, modern hair that feels like you
+          Let a world leading skin colour expert{" "}
+          <a href="/">
+            <mark> Codies </mark>
+          </a>
+          help create natural, <br /> glossy, modern hair that feels like you
         </h6>
 
         <div className="img-products">
           <div className="pro" style={{ backgroundColor: "purple" }}>
-            <img src="assets/pro3/anti-age.png" alt="anti-aging" />
-            <span></span>
-            <h3>Anti-Aging facial oil</h3>
-            <p>Codies beauty channel, anti aging facial oil.</p>
+            <ProDetails
+              proName="Anti-Aging facial oil"
+              proImg="assets/pro3/anti-age.png"
+              proDetails="Codies beauty channel, anti aging facial oil."
+              proAlt="anti-aging"
+            />
           </div>
           <div className="pro">
-            <img src="assets/pro3/facial.png" alt="bank" />
-            <span></span>
-            <h3>Carrot facial soap</h3>
-            <p>Codies beauty channel, carrot facial soap.</p>
+            <ProDetails
+              proName="Carrot facial soap"
+              proImg="assets/pro3/facial.png"
+              proDetails="Codies beauty channel, carrot facial soap."
+              proAlt="Carrot facial soap"
+            />
           </div>
+
           <div className="pro" style={{ backgroundColor: "orangered" }}>
-            <img src="assets/pro3/black.png" alt="bank" />
-            <span></span>
-            <h3>African Black Soap</h3>
-            <p>Codies beauty channel, anti aging facial oil.</p>
+            <ProDetails
+              proName="African Black Soap"
+              proImg="assets/pro3/black.png"
+              proDetails="Codies beauty channel, anti aging facial oil."
+              proAlt="African Black Soap"
+            />
+          </div>
+          <div className="pro" style={{ backgroundColor: "purple" }}>
+            <ProDetails
+              proName="Carrot facial soap"
+              proImg="assets/pro3/facial.png"
+              proDetails="Codies beauty channel, anti aging facial oil."
+              proAlt="African Black Soap"
+            />
           </div>
         </div>
       </div>
+    </>
+  );
+}
+
+function ProDetails(props) {
+  return (
+    <>
+      <img src={props.proImg} alt={props.proAlt} />
+      <span></span>
+      <h3>{props.proName}</h3>
+      <p>{props.proDetails}</p>
     </>
   );
 }
