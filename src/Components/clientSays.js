89168e0bd23dfbab86d57c735ac4248b0732d217
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-05-21 22:29:27 +0100

    okay

--- src/Components/clientSays.js
+++ src/Components/clientSays.js
@@ -1,32 +1,20 @@
 function ClientsSays() {
   return (
     <>
-      <div className="clitd">
-        <div className="videoDiv">
-          <ClientDetails com="COMMUNITY" />
-        </div>
-        <div className="videDiv">
-          <ClientDetails videoSRC="./assets/pro3/codio.mp4" />
-        </div>
-
-        <video src="./assets/pro3/codio.mp4" autoPlay>
-          {" "}
-          hello
-        </video>
+      <div className="manSays">
+        <h1>Community</h1>
       </div>
     </>
   );
 }
 
-function ClientDetails(props) {
-  <>
-    {/* <div className="manSays">
-      <h6>{props.com}</h6>
-      <video src={props.videoSRC}></video>
-    </div> */}
-
-    <h1>Hello clients</h1>
-  </>;
-}
+// function ClientDetails(props) {
+//   <>
+//     <div className="manSays">
+//       <h6>{props.com}</h6>
+//       <video src={props.videoSRC}></video>
+//     </div>
+//   </>;
+// }
 
 export default ClientsSays;
