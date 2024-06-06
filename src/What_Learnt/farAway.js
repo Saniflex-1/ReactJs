00253d0087d707942a7299c2265a03e3b8c634a0
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-06-07 00:50:17 +0100

    Create farAway.js

---
+++ src/What_Learnt/farAway.js
@@ -0,0 +1,15 @@
+function FarAways() {
+  return (
+    <div>
+      <div className="trips">
+        <div className="header">
+          <h2>Logo</h2>
+          <h1>FAR AWAY</h1>
+          <h3>Enjoy your trips</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FarAways;
