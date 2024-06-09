812557aa5cbc16ba58b828869c70933b8b6a80e3
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-06-09 02:32:04 +0100

    Update farAway.js

--- src/What_Learnt/farAway.js
+++ src/What_Learnt/farAway.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   {
     id: 1,
@@ -38,21 +40,40 @@ function Logo() {
   );
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    setDescription("");
+    setQuantity(1);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
   }
 
   return (
     <form className="form" onSubmit={handleSubmit}>
       <h2>What do you need for your 💕 trip</h2>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input text placeholder="Item...." />
+      <input
+        text
+        placeholder="Item...."
+        value={description}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
       <button>ADD</button>
     </form>
   );
